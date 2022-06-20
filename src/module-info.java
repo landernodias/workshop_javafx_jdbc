4b74e8e8512f0be73b69fa9c2ed1f287b361e070
Nelcione Dias <nelcionedias@gmail.com>
2022-06-20 11:52:57 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module workshop_javafx_jdbc {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.fxml, javafx.controls;
 }
