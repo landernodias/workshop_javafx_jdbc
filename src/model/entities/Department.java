3751d9f462aecfdf0cb13dc82a1491f5d24b0246
Nelcione Dias <nelcionedias@gmail.com>
2022-06-23 12:46:11 -0300

    Addin databese access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,12 +1,11 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	
@@ -36,7 +35,10 @@ public class Department implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -48,7 +50,12 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,11 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires java.sql;	
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
 	opens gui to javafx.fxml, javafx.controls;
 	opens gui.util to javafx.controls;
 	opens model.entities to javafx.base;
+	
 }
