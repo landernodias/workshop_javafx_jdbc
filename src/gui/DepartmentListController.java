1ff02824e655971736274028363087762ff3e71d
Nelcione Dias <nelcionedias@gmail.com>
2022-06-24 13:26:43 -0300

    Update department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -39,6 +41,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
+	
+	@FXML
+	private TableColumn<Department, Department> tableColumnEDIT;
 	
 	@FXML
 	private Button btNew;
@@ -81,6 +86,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 		//carregando a lista no observableList
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);//coloca na tableview
+		initEditButtons();// acrescenta novo botão com texto edit em cada linha da tabela
 	}
 	
 	//chama a janela para prencher/cadastrar um novo departamento
@@ -114,6 +120,29 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	public void onDataChanged() {
 		//executa quando os dados são alterado
 		updateTableView();
+	}
+	
+	//função do button edit
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>(){//instância e configura o evento do botão
+			private final Button button = new Button("edit");
+			
+			@Override
+			protected void updateItem(Department obj, boolean empty) {//obj: department da linha clicada
+				super.updateItem(obj, empty);
+			
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));// gera a janela do forms com objeto preenchido
+			}
+			
+		});
 	}
 
 }
